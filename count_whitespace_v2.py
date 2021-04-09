864dc25ec177286c4015babe2a707edd80a9d99d
tianniezing <78558772+tianniezing@users.noreply.github.com>
2021-04-09 15:06:27 +0200

    Add files via upload

---
+++ count_whitespace_v2.py
@@ -0,0 +1,30 @@
+import re
+
+def file_opener():
+    with open('mi.txt') as f:
+        return f.readlines()
+
+
+def count_whitespace1():
+    """Counts the whitespaces of every line in a text file."""
+    string = file_opener()
+    for i in string:
+        count_indent = len(i) - len(i.lstrip())
+        if count_indent == 26:
+            print('C|', i)
+        elif count_indent == 16:
+            print('D|', i)
+        elif count_indent == 5 and i.isupper():
+            print('S|', i)
+        elif count_indent == 5 and not i.isupper():
+            print('N|', i)
+        elif i.strip() == 'CUT TO:' or i.strip() == 'DISSOLVE:':
+            print('M|', i)
+        elif i.strip().startswith('(') and i.strip().endswith(')'):
+            print('M|', i)
+        else:
+            print(' |', i)
+
+
+if __name__ == '__main__':
+    count_whitespace1()
